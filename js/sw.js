47a539be9855cd40b6d57e9045cdd252dc8ea6a4
pia smith <basiccreation@yahoo.com>
2018-06-08 20:16:57 -0700

    registered service worker

--- js/sw.js
+++ js/sw.js
@@ -1,1 +1,14 @@
-sw.js
+/*service worker for restaurant viewer*/
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('js/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
