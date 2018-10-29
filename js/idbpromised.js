0da56211f4d973186f927140bda8caff412065bd
pia smith <basiccreation@yahoo.com>
2018-10-28 22:22:40 -0700

    added three object stores

--- js/idbpromised.js
+++ js/idbpromised.js
@@ -9,12 +9,24 @@
     if ('indexedDB' in window) {
       console.log('This browser supports IndexedDB');
     }
-  
+
   if (!('indexedDB' in window)) {
     console.log('This browser doesn\'t support IndexedDB');
     return;
   }
 
-  var dbPromise = idb.open('test-db1', 1);
-
-})();
+ var dbPromise = idb.open('test-db2', 1, function(upgradeDb) {
+    console.log('making first object store');
+    if (!upgradeDb.objectStoreNames.contains('firstOS')) {
+      upgradeDb.createObjectStore('firstOS');
+    } // end if ... secondOS
+        console.log('making second object store');
+    if (!upgradeDb.objectStoreNames.contains('secondOS')) {
+      upgradeDb.createObjectStore('secondOS');
+    } // end if ... secondOS
+        console.log('making third object store');
+    if (!upgradeDb.objectStoreNames.contains('thirdOS')) {
+      upgradeDb.createObjectStore('thirdOS');
+    } // end if ... thirdOS
+  }); // end dbPromise
+})(); // end anonymos function
