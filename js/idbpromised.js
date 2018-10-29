e1abe193a8c868ec740ad74ea10de34f90ad151c
pia smith <basiccreation@yahoo.com>
2018-10-28 21:58:41 -0700

    taking indexeddb off again. Set up idb

--- js/idbpromised.js
+++ js/idbpromised.js
@@ -1,3 +1,20 @@
-import idb from 'idb';
 
-await idb.open(…);
+
+
+
+(function() {
+  'use strict';
+
+  //check for support
+    if ('indexedDB' in window) {
+      console.log('This browser supports IndexedDB');
+    }
+  
+  if (!('indexedDB' in window)) {
+    console.log('This browser doesn\'t support IndexedDB');
+    return;
+  }
+
+  var dbPromise = idb.open('test-db1', 1);
+
+})();
