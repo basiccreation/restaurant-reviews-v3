06ec13f821bf0d11faa04d91f25fccede218bf92
pia smith <basiccreation@yahoo.com>
2018-10-28 18:47:29 -0700

    adding indexeddb for the restaurants again

--- js/dbindex_restaurants.js
+++ js/dbindex_restaurants.js
@@ -8,6 +8,16 @@
 //https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 //https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
 
+"use strict";
+
+//window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+
+//Inspiration was taken from 
+//https://www.youtube.com/watch?v=3y_x3Nkc8Dw PothOnProgramming YouTube Video
+//https://stackoverflow.com/questions/12607251/how-do-i-store-json-objects-in-indexeddb
+//https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+//https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
+
 var db;
 
 if (!window.indexedDB) {
@@ -18,11 +28,14 @@ if (window.indexedDB) {
     console.log("yay! indexedDB present");
 }; //end if indexedDB
 
-const request = window.indexedDB.open("database", 2);
+
+let request = window.indexedDB.open("RestaurantDatabase", 2);
+
 
 request.onerror = function(e) {
-    console.log("there was an error " + e.target.errorCode);
+    console.log("there was an error" + e.target.errorCode);
 }; //end onerror
+
 
 request.onsuccess = function(e) {
     db = e.target.result;
@@ -34,20 +47,14 @@ request.onupgradeneeded = function(e) {
     var db = e.target.result;
 
     //create object store for this database
-    var restaurantStore     = db.createObjectStore("restaurants", 
-        { autoIncrement: true });
-    var reviewStore         = db.createObjectStore("restaurantReviews", 
-        { autoIncrement: true });
+    var objectStore = db.createObjectStore("restaurants", { autoIncrement: true });
 
-    //create index to look up restaurants by favorite
-    // favoriteStore.createIndex("is_favorite", "is_favorite", 
-    //     { unique: false });
-    reviewStore.createIndex("restaurant_id", "restaurant_id", 
-        { unique: false });    
-    restaurantStore.createIndex("restaurant_id", "restaurant_id", 
-        { unique: false });
+    //create indexes to look up restaurants by
+    objectStore.createIndex("name",         "name"        , { unique: false });
+    objectStore.createIndex("neighborhood", "neighborhood", { unique: false });
+    objectStore.createIndex("cuisine_type", "cuisine_type", { unique: false });
 
-    restaurantStore.transaction.oncomplete = function(e) {
+    objectStore.transaction.oncomplete = function(e) {
 
         const JSONurl = "http://localhost:1337/restaurants";
 
@@ -56,36 +63,16 @@ request.onupgradeneeded = function(e) {
                 return response.json();
             })
             .then(function(resturantsJSON) {
-                //var transaction = db.transaction("restaurants", "readwrite");
+                var transaction = db.transaction("restaurants", "readwrite");
                 var objstore = transaction.objectStore("restaurants");
                 for (var i = 0; i < resturantsJSON.length; i++) {
-                    restaurantStore.add(resturantsJSON[i])
+                    objstore.add(resturantsJSON[i])
                 }
             })
             .catch(err => {
-                console.log("resturant JSON err");
+                console.log("JSON err");
             }); // end fetch
-    } // end restaurants objectstore
-
-    reviewStore.transaction.oncomplete = function(e) {
-
-        const JSONurl = "http://localhost:1337/reviews";
-
-        fetch(JSONurl)
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(resturantsJSON) {
-                // var transaction = db.transaction("restaurantReviews", "readwrite");
-                var objstore = transaction.objectStore("restaurantReviews");
-                for (var i = 0; i < resturantsJSON.length; i++) {
-                    reviewStore.add(resturantsJSON[i])
-                }
-            })
-            .catch(err => {
-                console.log("resturant JSON err");
-            }); // end fetch
-    }  // end restaurants reviewStore
+    }// end restaurants objectstore
 
 
-} // onupgradeneeded
+};//end onupgradeneeded
